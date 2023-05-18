58e024a31e7eec79f55f970dc7c70d9f893e7961
JeanRose-JAC <98351000+JeanRose-JAC@users.noreply.github.com>
2023-05-17 20:43:06 -0400

    getting breakfast recipes implemented

--- bakery-frontend/src/components/GetAllBreakfastRecipes.js
+++ bakery-frontend/src/components/GetAllBreakfastRecipes.js
@@ -1,5 +1,5 @@
 import { useEffect,useState } from "react";
-import { ListRecipes } from "./ListBreakfastRecipes ";
+import { ListRecipesFromSearch } from "./ListRecipesFromSearch";
 
 /**
  * Displays all of the recipes in the database
@@ -16,12 +16,20 @@ function GetAllBreakfastRecipes(){
     return(
         <div className="center">
             <h1> recipes</h1>
-            {recipe[0] && <ListRecipes recipes={recipe}/>}
+            {recipe[0] && <ListRecipesFromSearch recipes={recipe}/>}
         </div>
     );
 }
 async function GetAllBreakfastRecipesControl(setRecipe){
-    const response = await fetch ("http://localhost:1339/recipe/", {method: "GET"})
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "X-Api-Key" : "9tMQWPbv5v9Bs46FPlhgOw==Fq96b3KRP2L1EGdG"
+        },
+    }
+
+    const response = await fetch("https://api.api-ninjas.com/v1/recipe?query=breakfast", requestOptions);
     const result = await response.json();
     setRecipe(result);
 
