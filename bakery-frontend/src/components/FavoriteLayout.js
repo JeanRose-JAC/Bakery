da65349e5fcdb503eccf87a6697cc22871129045
JeanRose-JAC <98351000+JeanRose-JAC@users.noreply.github.com>
2023-05-18 17:31:57 -0400

    removed unused code

--- bakery-frontend/src/components/FavoriteLayout.js
+++ bakery-frontend/src/components/FavoriteLayout.js
@@ -1,37 +1,3 @@
-// import { useState } from "react";
-// import { ListRecipes } from "./ListRecipes";
-
-// /**
-//  * Displays all of the recipes in the database
-//  * 
-//  * @returns A list of all the recipes
-//  */
-// function GetAllRecipes(){
-//     const[recipe, setRecipe] = useState([]);
-
-
-//     const callGetAllRecipe  = async () => {
-//         const response = await fetch ("http://localhost:1339/recipe/", {method: "GET"})
-//         const result = await response.json();
-//         setRecipe(result);
-
-//     }
-    
-
-//     return(
-//         <div className="center">
-//             <h1>Saved Recipes</h1>
-//             <button onClick={callGetAllRecipe} className="buttonIngredients">Get Saved Recipes</button>
-//             <button onClick={callGetAllRecipe} className="buttonBook">Get All ingredients</button>
-//             <button onClick={callGetAllRecipe} className="buttonBook">My recipes</button>  
-//             {recipe[0] && <ListRecipes recipes={recipe}/>}
-//         </div>
-//     );
-
-// }
-
-// export {GetAllRecipes};
-
 import { useContext } from 'react';
 import { LoggedInContext } from './App';
 import { Link } from 'react-router-dom';
